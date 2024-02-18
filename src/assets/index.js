8455242e0faad86879ac803299297c56156409b2
EhsanKINUX <ehsankhwentao@gmail.com>
2024-02-18 19:30:22 +0330

    feat: add and import new images

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import img1 from "../assets/images/1.png";
+import img2 from "../assets/images/2.jpg";
+import img3 from "../assets/images/3.png";
+import img4 from "../assets/images/4.jpg";
+import img5 from "../assets/images/5.png";
+import img6 from "../assets/images/6.jpeg";
+import img7 from "../assets/images/7.jpeg";
+import img8 from "../assets/images/8.jpeg";
+import img9 from "../assets/images/9.png";
+import img10 from "../assets/images/10.jpeg";
+import img11 from "../assets/images/11.jpeg";
+import img12 from "../assets/images/12.png";
+// import img13 from "../assets/images/13.jpg";
+// import img14 from "../assets/images/14.jpg";
+// import img15 from "../assets/images/15.jpg";
+
+export { img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12 };
