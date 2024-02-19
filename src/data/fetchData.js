3ed507e115a93123c4fbe1563294c48110ded06e
EhsanKINUX <ehsankhwentao@gmail.com>
2024-02-19 13:12:42 +0330

    fix: comment console log

--- src/data/fetchData.js
+++ src/data/fetchData.js
@@ -8,7 +8,7 @@ export const fetchData = async () => {
       return { url: item.picture, width: item.width, height: item.height, id: item.id, nftName: item.nftName, nftLink: item.nftLink, artist: item.artist };
     });
 
-    console.log(pictures);
+    // console.log(pictures);
     return pictures;
   } catch (error) {
     console.error("Error fetching data: ", error);
