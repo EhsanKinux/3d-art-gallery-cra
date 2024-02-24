e0b09ff8ab61e8ef38517fd8c09acb9ea621792b
EhsanKINUX <ehsankhwentao@gmail.com>
2024-02-24 15:29:01 +0330

    feat: create squareChecker for the movement positions of the pictures

---
+++ src/components/canvas/parts/parts/squareMovementPosition.js
@@ -0,0 +1,40 @@
+export function squareChecker(zCamera, squares) {
+  let minimalDistance = 1;
+  squares.forEach((square, index) => {
+    const { z } = square.position;
+    const deltaZ = zCamera - z;
+    if (deltaZ <= minimalDistance && deltaZ > 0) {
+      const diffZ = 2 * (minimalDistance - deltaZ);
+      // Determine the corner for this square
+      const corner = index % 4; // This will give us a value from 0 to 3
+      let targetX, targetY;
+      switch (corner) {
+        case 0: // Top-left
+          targetX = square.initX - diffZ;
+          targetY = square.initY + diffZ;
+          break;
+        case 1: // Top-right
+          targetX = square.initX + diffZ;
+          targetY = square.initY + diffZ;
+          break;
+        case 2: // Bottom-left
+          targetX = square.initX - diffZ;
+          targetY = square.initY - diffZ;
+          break;
+        case 3: // Bottom-right
+          targetX = square.initX + diffZ;
+          targetY = square.initY - diffZ;
+          break;
+        default:
+          targetX = square.initX;
+          targetY = square.initY;
+          console.warn(`Unexpected corner value: ${corner}. Resetting square to initial position.`);
+          break;
+      }
+
+      // Apply the calculated target positions
+      square.position.x = targetX;
+      square.position.y = targetY;
+    }
+  });
+}
