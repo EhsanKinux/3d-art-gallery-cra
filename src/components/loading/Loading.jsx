eb833ba015b93f1c34f176654c73a72a6e98dc33
EhsanKINUX <ehsankhwentao@gmail.com>
2024-02-24 18:02:42 +0330

    fix: ui bug fixed

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -7,7 +7,7 @@ const Loading = () => {
       className={`fixed w-screen h-screen flex flex-col items-center justify-center z-[99] bg-black gap-6 opacity-50`}
     >
       <div className="w-screen h-screen flex items-center justify-center gap-2">
-        <span className="text-lg font-bold text-white gap-6">LOADING PICTURES...</span>
+        <span className="text-lg font-bold text-white gap-6 shadow-yellow-50 bg-slate-800 rounded-md p-2">LOADING PICTURES...</span>
         {/* <span className="font-bold loading loading-spinner text-warning loading-lg"></span> */}
       </div>
     </div>
